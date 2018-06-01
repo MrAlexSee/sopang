6c4466da4fb32ff6585a7d0b5e0d6f3d78836f12
Aleksander Cisłak <alex.cis91@gmail.com>
2018-06-01 22:02:09 +0200

    Fixed unit tests makefile dependency bug, added first unit tests for filling mask buffer.

--- unit_tests/sopang_tests.cpp
+++ unit_tests/sopang_tests.cpp
@@ -22,6 +22,10 @@ public:
     {
         sopang.fillPatternMaskBuffer(arg1, arg2);
     }
+    inline static uint64_t *getMaskBuffer(Sopang &sopang)
+    {
+        return sopang.maskBuffer;
+    }
 };
 
 namespace
@@ -53,13 +57,25 @@ TEST_CASE("is parsing patterns multiple empty and trailing correct", "[parsing]"
     vector<string> patterns = Sopang::parsePatterns(str);
 
     REQUIRE(patterns.size() == 5);
-    REQUIRE(Helpers::join(patterns, "") == "12345");
+    REQUIRE(Helpers::join(patterns, "") == "12345" );
 }
 
 TEST_CASE("is filling mask buffer correct", "[matching]")
 {
     Sopang sopang;
-    SopangWhitebox::fillPatternMaskBuffer(sopang, "AAAA", alphabet);
+    SopangWhitebox::fillPatternMaskBuffer(sopang, "AAAAA", alphabet);
+
+    uint64_t *maskBuffer = SopangWhitebox::getMaskBuffer(sopang);
+
+    for (uint64_t m = 0x1; m <= 0x5; ++m)
+    {
+        REQUIRE((maskBuffer['A'] & m) == 0x0);
+
+        REQUIRE((maskBuffer['C'] & m) != 0x0);
+        REQUIRE((maskBuffer['G'] & m) != 0x0);
+        REQUIRE((maskBuffer['T'] & m) != 0x0);
+        REQUIRE((maskBuffer['N'] & m) != 0x0);
+    }
 }
 
 }
