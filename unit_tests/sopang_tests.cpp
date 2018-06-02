484360cea4363f0496cd03cffbb46bc4260c5297
Aleksander Cisłak <alex.cis91@gmail.com>
2018-06-02 11:32:14 +0200

    Unit tests for pattern mask constructions, added .gitignore to ignore object files.

--- unit_tests/sopang_tests.cpp
+++ unit_tests/sopang_tests.cpp
@@ -31,9 +31,10 @@ public:
 namespace
 {
 const string alphabet = "ACGTN";
+constexpr int maxPatSize = 64;
 }
 
-TEST_CASE("is parsing patterns single correct", "[parsing]")
+TEST_CASE("is parsing patterns for a single pattern correct", "[parsing]")
 {
     string str = "1";
     vector<string> patterns = Sopang::parsePatterns(str);
@@ -51,7 +52,7 @@ TEST_CASE("is parsing patterns correct", "[parsing]")
     REQUIRE(Helpers::join(patterns, "") == "12345");
 }
 
-TEST_CASE("is parsing patterns multiple empty and trailing correct", "[parsing]")
+TEST_CASE("is parsing patterns correct for multiple empty and trailing lines", "[parsing]")
 {
     string str = "\n\n\n\n1\n\n2\n3\n4\n\n\n5\n\n\n\n\n\n";
     vector<string> patterns = Sopang::parsePatterns(str);
@@ -60,21 +61,65 @@ TEST_CASE("is parsing patterns multiple empty and trailing correct", "[parsing]"
     REQUIRE(Helpers::join(patterns, "") == "12345" );
 }
 
-TEST_CASE("is filling mask buffer correct", "[matching]")
+TEST_CASE("is filling mask buffer correct for a predefined pattern", "[matching]")
 {
     Sopang sopang;
-    SopangWhitebox::fillPatternMaskBuffer(sopang, "AAAAA", alphabet);
+    SopangWhitebox::fillPatternMaskBuffer(sopang, "ACAACGT", alphabet);
 
     uint64_t *maskBuffer = SopangWhitebox::getMaskBuffer(sopang);
 
-    for (uint64_t m = 0x1; m <= 0x5; ++m)
-    {
-        REQUIRE((maskBuffer['A'] & m) == 0x0);
+    uint64_t maskA = maskBuffer[static_cast<size_t>('A')];
 
-        REQUIRE((maskBuffer['C'] & m) != 0x0);
-        REQUIRE((maskBuffer['G'] & m) != 0x0);
-        REQUIRE((maskBuffer['T'] & m) != 0x0);
-        REQUIRE((maskBuffer['N'] & m) != 0x0);
+    REQUIRE((maskA & static_cast<uint64_t>(0x1)) == 0x0);
+    REQUIRE((maskA & static_cast<uint64_t>(0x1 << 1)) != 0x0);
+    REQUIRE((maskA & static_cast<uint64_t>(0x1 << 2)) == 0x0);
+    REQUIRE((maskA & static_cast<uint64_t>(0x1 << 3)) == 0x0);
+    REQUIRE((maskA & static_cast<uint64_t>(0x1 << 4)) != 0x0);
+    REQUIRE((maskA & static_cast<uint64_t>(0x1 << 5)) != 0x0);
+    REQUIRE((maskA & static_cast<uint64_t>(0x1 << 6)) != 0x0);
+
+    uint64_t maskC = maskBuffer[static_cast<size_t>('C')];
+
+    uint64_t maskG = maskBuffer[static_cast<size_t>('G')];
+
+    uint64_t maskT = maskBuffer[static_cast<size_t>('T')];
+
+    uint64_t maskN = maskBuffer[static_cast<size_t>('N')];
+
+
+}
+
+TEST_CASE("is filling mask buffer correct for repeated same character in pattern", "[matching]")
+{
+    for (const char c : alphabet)
+    {
+        for (int size = 1; size <= maxPatSize; ++size)
+        {
+            string pattern = "";
+            repeat(size, [c, &pattern]() { pattern += c; });
+
+            Sopang sopang;
+            SopangWhitebox::fillPatternMaskBuffer(sopang, pattern, alphabet);
+
+            uint64_t *maskBuffer = SopangWhitebox::getMaskBuffer(sopang);
+            uint64_t m = 0x1;
+
+            for (int shift = 0; shift < size; ++shift)
+            {
+                for (const char curC : alphabet)
+                {
+                    if (curC == c) // Corresponding occurrences should be set to 0.
+                    {
+                        REQUIRE((maskBuffer[static_cast<size_t>(curC)] & m) == 0x0);
+                    }
+                    else
+                    {
+                        REQUIRE((maskBuffer[static_cast<size_t>(curC)] & m) != 0x0);
+                    }
+                }
+                m <<= 1;
+            }
+        }
     }
 }
 
