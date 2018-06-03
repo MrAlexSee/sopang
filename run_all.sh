dca38fa59d0b1ea4d55ebce24f33f5e08d7c94c8
Aleksander Cisłak <alex.cis91@gmail.com>
2018-06-03 21:44:18 +0200

    Added missing input dir prefix to pattern files in run_all.sh.

--- run_all.sh
+++ run_all.sh
@@ -5,8 +5,8 @@ outFile="out"
 
 for i in $(seq 1 27);
 do
-    ./sopang -d -o ${outFile}8.txt $inputDir/chr${i}.eds patterns8.txt;
-    ./sopang -d -o ${outFile}16.txt $inputDir/chr${i}.eds patterns16.txt;
-    ./sopang -d -o ${outFile}32.txt $inputDir/chr${i}.eds patterns32.txt;
-    ./sopang -d -o ${outFile}64.txt $inputDir/chr${i}.eds patterns64.txt;
+    ./sopang -d -o ${outFile}8.txt $inputDir/chr${i}.eds $inputDir/patterns8.txt;
+    ./sopang -d -o ${outFile}16.txt $inputDir/chr${i}.eds $inputDir/patterns16.txt;
+    ./sopang -d -o ${outFile}32.txt $inputDir/chr${i}.eds $inputDir/patterns32.txt;
+    ./sopang -d -o ${outFile}64.txt $inputDir/chr${i}.eds $inputDir/patterns64.txt;
 done
