392712a475373ccb4e98e06883223a0f1d8204d3
Aleksander Cisłak <alex.cis91@gmail.com>
2018-06-07 11:20:44 +0200

    Added a script for character counting in .eds files.

---
+++ scripts/ed_histogram.py
@@ -0,0 +1,32 @@
+"""
+Prints the histogram of character counts in determinate/indeterminate segments 
+for all .eds files from a given folder.
+"""
+
+import os
+import re
+
+inFolder = "../sample" # Input folder path.
+
+def main():
+    res = []
+    edsFiles = [os.path.join(inFolder, f) for f in os.listdir(inFolder) if f.endswith(".eds")]
+
+    for edsFile in edsFiles:
+        with open(edsFile, "r") as f:
+            data = f.read()
+        
+        # The question mark allows for non-greedy matching.
+        detFiltered = re.sub(r"\{.+?\}", "", data)
+
+        sizeMB = len(data) / 1024.0 / 1024.0
+        sizeFiltMB = len(detFiltered) / 1024.0 / 1024.0
+        ratio = sizeFiltMB / sizeMB
+
+        res += [(edsFile, round(sizeFiltMB, 2), round(sizeMB, 2), round(ratio, 2))]
+
+    for t in res:
+        print t
+
+if __name__ == "__main__":
+    main()
