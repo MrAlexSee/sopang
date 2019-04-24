7fbab6ab3832a588a6f1c318bf4b4aec23561944
MrAlexSee <alex.cis91@gmail.com>
2019-04-24 16:26:55 +0200

    First attempt at parsing fasta-vcf pair.

--- scripts/parse_fasta_vcf_to_ed_sources.py
+++ scripts/parse_fasta_vcf_to_ed_sources.py
@@ -1,54 +1,149 @@
 """
 Parses a fasta and VCF file pair in order to obtain elastic-degenerate text with sources.
+Usage: parse_fasta_vcf_to_ed_sources.py <input_chr.fa> <input_variants.vcf> <output_chr.eds> <output_sources.edss>
+
+Arguments:
+  <input_chr.fa>        path to the input fasta (reference) file
+  <input_variants.vcf>  path to the input vcf (variants) file
+  <output_chr.eds>      path to the output elastic-degenerate text file
+  <output_sources.edss> path to the output sources file
+
+Options:
+  -h --help       show this screen
+  -v --version    show version
 """
 
+from docopt import docopt
+
 import itertools
-import vcf
+import vcf # Requires the package "pyvcf".
 
-# Input fasta file path.
-pInFastaFilePath = "/home/alex/Documents/Chromosomes/danio_rerio_chr1.fa"
+def readFastaFile(inFastaFilePath):
+    print "Reading the entire fasta file from: {0}".format(inFastaFilePath)
+    text = ""
 
-# Input VCF file path.
-pInVCFFilePath = "/home/alex/Documents/Chromosomes/danio_rerio.vcf"
-
-# Input chromosome ID.
-pChromosomeID = 1
-
-# Output elastic-degenerate text file path.
-pOutEDTextFilePath = "text.eds"
-
-# Output sources file path.
-pOutEDSourcesFilePath = "sources.edss"
-
-def parseFastaAndVCF(inFastaFilePath, inVCFFilePath, chromosomeID):
     with open(inFastaFilePath, "r") as f:
-        lines = [l for l in f.read().split("\n")[1 : ] if l]
+        for line in f:
+            if line[0] == ">":
+                continue
 
-    text = "".join(lines)
+            text += line[ : -1].upper()
+
     textMB = float(len(text)) / 1000.0 / 1000.0
+    print "Read input fasta text, MB = {0:.2f}".format(textMB)
 
-    print "Read text, MB = {0:.2f}".format(textMB)
+    return text
 
-    vcfReader = vcf.Reader(open(inVCFFilePath, "r"))
-    nProcessedEntries = 0
+def getSourcesMapFromVCF(vcfReader):
+    nProcessedEntries, nIgnoredEntries = 0, 0
+    ret = {}
+
+    sampleNameToIndex = {}
+    nextSampleIndex = 0
+
+    alphabet = set("ACGTN")
 
     for record in vcfReader:
-        chromID = int(record.CHROM)
+        ignoreEntry = False
 
-        if chromID == chromosomeID:
-            nProcessedEntries += 1
-            pass
-        elif chromID > chromosomeID:
-            break
+        for refChar in record.REF:
+            if refChar.upper() not in alphabet:
+                ignoreEntry = True
+                break
 
-    print "Processed vcf #entries = {0} for chromosome = {1}".format(nProcessedEntries, chromosomeID)
+        if ignoreEntry:
+            nIgnoredEntries += 1
+            continue
 
-    return [], []
+        for altSequence in record.ALT:
+            if type(altSequence) != vcf.model._Substitution:
+                ignoreEntry = True
+                break
+
+            for altChar in altSequence.sequence:
+                if altChar.upper() not in alphabet:
+                    ignoreEntry
+                    break
+
+            if ignoreEntry:
+                break
+
+        if ignoreEntry:
+            nIgnoredEntries += 1
+            continue
+
+        # We store only the sources where the variation (alt) occurs.
+        curSources = {}
+
+        for sample in record.samples:
+            # We always take only the 1st index of the diploid.
+            altIndex = int(sample.data.GT.split("|")[0])
+
+            if altIndex != 0:
+                assert 1 <= altIndex <= len(record.ALT)
+                altIndex -= 1
+
+                if record.ALT[altIndex] not in curSources:
+                    curSources[record.ALT[altIndex]] = []
+
+                sampleName = sample.sample
+
+                if sampleName not in sampleNameToIndex:
+                    sampleNameToIndex[sampleName] = nextSampleIndex
+                    nextSampleIndex += 1
+
+                curSources[record.ALT[altIndex]] += [sampleNameToIndex[sampleName]]
+
+        if curSources:
+            if record.POS in ret:
+                ret[record.POS].update(curSources)
+            else:
+                ret[record.POS] = curSources
+
+        nProcessedEntries += 1
+
+    print "Processed vcf #entries = {0}, ignored #entries = {1}".format(nProcessedEntries, nIgnoredEntries)
+    return ret, nextSampleIndex
+
+def fastaAndSourcesMapToSegments(fastaText, sourcesMap, nSources):
+    segments, sources = [], []
+
+    for charIdx in xrange(len(fastaText)):
+        if charIdx in sourcesMap:
+            curSegment, curSources = [], []
+            usedSources = set()
+
+            for kv in sourcesMap[charIdx].iteritems():
+                curSegment += [kv[0]]
+                curSources += [kv[1]]
+
+                usedSources.update(set(kv[1]))
+
+            # We associate the reference sequence with the remaining sources.
+            curSources += [[s for s in xrange(nSources) if s not in usedSources]]
+
+            curSegment = [[fastaText[charIdx]]]
+
+            segments += [curSegment]
+            sources += [curSources]
+        else:
+            segments += [[fastaText[charIdx]]]
+
+    return segments, sources
+
+def parseFastaAndVCF(inFastaFilePath, inVCFFilePath):
+    fastaText = readFastaFile(inFastaFilePath)
+    vcfReader = vcf.Reader(open(inVCFFilePath, "r"))
+
+    print "Starting buffered processing of vcf file from: {0}".format(inVCFFilePath)
+    sourcesMap, nSources = getSourcesMapFromVCF(vcfReader)
+
+    return fastaAndSourcesMapToSegments(fastaText, sourcesMap, nSources)
 
 def sanityCheck(segments, sources):
     assert segments and sources
 
-    iSource = 0
+    sourceIdx = 0
     nNonDeterministicSegments = 0
     
     for segment in segments:
@@ -57,53 +152,72 @@ def sanityCheck(segments, sources):
         if len(segment) == 1:
             continue
 
-        assert len(segment) == len(sources[iSource])
+        assert len(segment) == len(sources[sourceIdx])
 
         # Make sure that the are no duplicates in all sublists.
-        allSources = list(itertools.chain(*sources[iSource]))
+        allSources = list(itertools.chain(*sources[sourceIdx]))
         assert len(allSources) == len(set(allSources))
 
-        iSource += 1
+        sourceIdx += 1
         nNonDeterministicSegments += 1
 
     assert nNonDeterministicSegments == len(sources)
+    print "Sanity check passed"
 
 def dumpEDText(segments, outFilePath):
     text = ""
+    step = 100
 
-    for segment in segments:
+    print "Buffered dumping of ED text to: {0} for #segments = {1} with step = {2}".format(outFilePath, len(segments), step)
+    fileHandle = open(outFilePath, "a")
+
+    fileHandle.truncate()
+
+    for segmentIdx, segment in enumerate(segments):
+        if segmentIdx != 0 and segmentIdx % step == 0:
+            fileHandle.write(text)
+            text = ""
+
         if len(segment) == 1:
             text += "{" + segment[0] + "}"
         else:
             text += "{" + ",".join(segment) + "}"
 
-    with open(outFilePath, "w") as f:
-        f.write(text)
-
-    print("Dumped ed text to: {0} for #segments = {1}".format(outFilePath, len(segments)))
+    fileHandle.close()
+    print "Finished"
 
 def dumpSources(sources, outFilePath):
     text = ""
+    step = 100
 
-    for sourcesForSegment in sources:
-        textsForSegment = []
+    print "Buffered dumping of sources to: {0} for non-det #segments = {1} with step = {2}".format(outFilePath, len(sources), step)
+    fileHandle = open(outFilePath, "a")
+
+    fileHandle.truncate()
+
+    for sourcesIdx, sourcesForSegment in enumerate(sources):
+        if segmentIdx != 0 and segmentIdx % step == 0:
+            fileHandle.write(text)
+            text = ""
+
+        textForSegment = []
 
         for sourcesForVariant in sourcesForSegment:
-            textsForSegment += ["{" + ",".join([str(v) for v in sourcesForVariant]) + "}"]
+            textForSegment += ["{" + ",".join([str(v) for v in sourcesForVariant]) + "}"]
 
-        text += "{" + "".join(textsForSegment) + "}"
+        text += "{" + "".join(textForSegment) + "}"
 
-    with open(outFilePath, "w") as f:
-        f.write(text)
-
-    print("Dumped sources to: {0} for #segments = {1}".format(outFilePath, len(sources)))
+    fileHandle.close()
+    print "Finished"
 
 def main():
-    segments, sources = parseFastaAndVCF(pInFastaFilePath, pInVCFFilePath, pChromosomeID)
+    args = docopt(__doc__, version="0.1.0")
+
+    segments, sources = parseFastaAndVCF(args["<input_chr.fa>"], args["<input_variants.vcf>"])
     sanityCheck(segments, sources)
 
-    dumpEDText(segments, pOutEDTextFilePath)
-    dumpSources(sources, pOutEDSourcesFilePath)
+    dumpEDText(segments, args["<output_chr.eds>"])
+    dumpSources(sources, args["<output_sources.edss>"])
 
 if __name__ == "__main__":
     main()
