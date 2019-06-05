3580dd6862e3c28a1b115a39747fa8d1b0c7f6e0
MrAlexSee <alex.cis91@gmail.com>
2019-06-05 11:32:02 +0200

    Refactored a script for generating synthetic sources.

--- scripts/generate_synth_sources.py
+++ scripts/generate_synth_sources.py
@@ -2,50 +2,45 @@
 Generates a synthetic sources file for elastic-degenerate text.
 Each source (input individual) is associated with exactly one variant from each non-deterministic segment.
 Each variant is associated with some sources.
+
+Usage: generate_synth_sources.py <input-ed-file> <output-src-file> [options]
+
+Options:
+  --source-count arg           total number of sources (input individuals) [default: 10]
+  --include-last-sequence      dump sources for the last sequence (results in a longer but explicit sources file)
 """
+
+from docopt import docopt
 
 import random
 
-# Path to the input file with elastic-degenerate text for which the sources will be generated.
-pInEDTextFilePath = "../sample/chr1337.eds"
-
-# Path to the output sources file.
-pOutEDSourcesFilePath = "sources.edss"
-
-# Total number of sources (input individuals). Must be at least as large as the maximum number of variants in a non-deterministic segment in the input elastic-degenerate text.
-pNSources = 10
-
-# Does not dump sources for the last sequence in order to have a shorter sources file.
-pIgnoreLastSequence = True
-
 def readEDSegments(inFilePath):
     with open(inFilePath, "r") as f:
-        data = f.read()
+        text = f.read()
 
-    segments = []
+    ret = []
 
-    for part in data.split("{"):
-        # This is a deterministic segment.
-        if "}" not in part:
-            segments += [[part]]
-        else:
-            curParts = part.split("}")
-            assert len(curParts) == 2
+    for part in text.split("{"):
+        if "}" in part:
+            parts = part.split("}")
+            assert len(parts) == 2
 
             # This is a non-deterministic segment.
-            assert "," in curParts[0]
-            segments += [curParts[0].split(",")]
+            assert "," in parts[0]
+            ret += [parts[0].split(",")]
 
             # This is either a deterministic segment or an empty string
             # when non-deterministic segments are contiguous.
-            assert "," not in curParts[1]
-            if curParts[1]:
-                segments += [[curParts[1]]]
+            assert "," not in parts[1]
+            if parts[1]:
+                ret += [[parts[1]]]
+        else:
+            ret += [[part]]
 
-    print "Parsed #segments = {0}".format(len(segments))
-    return segments
+    print("Parsed #segments = {0}".format(len(ret)))
+    return ret
 
-def generateSources(segments, nSources):
+def generateSources(segments, sourceCount, includeLast):
     sources = []
     
     for segment in segments:
@@ -53,45 +48,38 @@ def generateSources(segments, nSources):
         if len(segment) == 1:
             continue
 
-        if nSources < len(segment):
-            print "Not enough sources for segment: {0} < {1}".format(nSources, len(segment))
-            return []
+        if sourceCount < len(segment):
+            raise Exception("not enough sources for segment: {0} < {1}".format(sourceCount, len(segment)))
 
         sourcesForSegment = []
         
-        sourcesToSelect = [i for i in xrange(nSources)]
+        sourcesToSelect = [i for i in range(sourceCount)]
         random.shuffle(sourcesToSelect)
 
-        iStart = 0
-        iStep = int(nSources / len(segment))
+        startIdx = 0
+        stepIdx = int(sourceCount / len(segment))
 
-        for iVariant in xrange(len(segment)):
-            if iVariant != len(segment) - 1:
-                sourcesForVariant = sourcesToSelect[iStart : iStart + iStep]
-            else:
-                if not pIgnoreLastSequence:
-                    sourcesForVariant = sourcesToSelect[iStart : ]
-                else:
-                    sourcesForVariant = None
+        for variantIdx in range(len(segment)):
+            if variantIdx != len(segment) - 1:
+                sourcesForSegment += [sourcesToSelect[startIdx : startIdx + stepIdx]]
+            elif includeLast:
+                sourcesForSegment += [sourcesToSelect[startIdx : ]]
 
-            if sourcesForVariant:
-                sourcesForSegment += [sourcesForVariant]
-
-            iStart += iStep
-
-        if not pIgnoreLastSequence:
-            assert len(sourcesForSegment) == len(segment)
-            assert sum([len(s) for s in sourcesForSegment]) == nSources
-        else:
-            assert len(sourcesForSegment) == len(segment) - 1
-            assert sum([len(s) for s in sourcesForSegment]) < nSources
+            startIdx += stepIdx
 
         sources += [sourcesForSegment]
 
+        if includeLast:
+            assert len(sourcesForSegment) == len(segment)
+            assert sum([len(s) for s in sourcesForSegment]) == sourceCount
+        else:
+            assert len(sourcesForSegment) == len(segment) - 1
+            assert sum([len(s) for s in sourcesForSegment]) < sourceCount
+
     return sources
 
-def dumpSources(sources, outFilePath):
-    text = "{0}\n".format(pNSources)
+def dumpSources(sources, sourceCount, outFilePath):
+    text = "{0}\n".format(sourceCount)
 
     for sourcesForSegment in sources:
         textsForSegment = []
@@ -104,17 +92,19 @@ def dumpSources(sources, outFilePath):
     with open(outFilePath, "w") as f:
         f.write(text)
 
-    print "Dumped sources to: {0} for non-det #segments = {1}".format(outFilePath, len(sources))
+    print("Dumped sources to: {0}, #sources = {1}".format(outFilePath, sourceCount))
 
 def main():
-    segments = readEDSegments(pInEDTextFilePath)
-    sources = generateSources(segments, pNSources)
+    args = docopt(__doc__, version="0.1.0")
+    sourceCount = int(args["--source-count"])
 
-    if not sources:
-        return
+    segments = readEDSegments(args["<input-ed-file>"])
+    sources = generateSources(segments, sourceCount, args["--include-last-sequence"])
 
-    print "Non-det/det segment ratio = {0:.2f}".format(float(len(sources)) / len(segments))
-    dumpSources(sources, pOutEDSourcesFilePath)
+    ratio = len(sources) / len(segments)
+    print("Non-det/det segment ratio = {0:.2f}".format(ratio))
+
+    dumpSources(sources, sourceCount, args["<output-src-file>"])
 
 if __name__ == "__main__":
     main()
