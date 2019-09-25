d2975ba1b3d90111cd3dada9a0b903b5ddbece69
MrAlexSee <alex.cis91@gmail.com>
2019-09-25 21:08:50 +0200

    Added handling empty indexes and extended test cases.

--- prototypes/match_sources_prototype.py
+++ prototypes/match_sources_prototype.py
@@ -56,7 +56,7 @@ def verify(text, sources, pattern, indexToSourceIndex, indexToMatch):
         while leafList and segmentIndex >= 0:
             assert nx.is_directed_acyclic_graph(matchTree)
 
-            print(f"Segment: {segmentIndex} -> #leaves = {len(leafList)}")
+            print(f"Segment: {segmentIndex} (len = {len(text[segmentIndex])}) -> #leaves = {len(leafList)}")
             newLeafList = []
 
             # Each time we traverse a segment, we need to update every leaf.
@@ -69,7 +69,7 @@ def verify(text, sources, pattern, indexToSourceIndex, indexToMatch):
                     matchTree.node[leaf]["pattern_char_index"] -= len(text[segmentIndex][0])
 
                     if matchTree.node[leaf]["pattern_char_index"] < 0:
-                        print("  Match ends in the middle of the variant")
+                        print("  Match ends within the variant")
                         matchTree.node[leaf]["match_complete"] = True
 
                         if leaf == rootNode:
@@ -78,16 +78,25 @@ def verify(text, sources, pattern, indexToSourceIndex, indexToMatch):
                         newLeafList += [leaf]
                 else:
                     for variantIndex, variant in enumerate(text[segmentIndex]):
-                        print(f"  Checking variant: {variantIndex} -> {variant}")
                         matchNode = (segmentIndex, variantIndex)
 
+                        if len(variant) == 0:
+                            print(f"  Checking variant: {variantIndex} -> empty")
+
+                            matchTree.add_node(matchNode, pattern_char_index = matchTree.node[leaf]["pattern_char_index"])
+                            matchTree.add_edge(leaf, matchNode)
+
+                            newLeafList += [matchNode]
+                            continue
+
+                        print(f"  Checking variant: {variantIndex} -> {variant}")
                         curCharIndex = len(variant) - 1
                         curPatternIndex = matchTree.node[leaf]["pattern_char_index"]
 
                         while curCharIndex >= 0:
-                            # A match in the middle of this variant -> add a new match complete node.
                             if curPatternIndex < 0:
-                                print("  Match ends in the middle of the variant")
+                                # A match within this variant -> add a new match complete node.
+                                print("  Match ends within the variant")
 
                                 matchTree.add_node(matchNode, match_complete=True)
                                 matchTree.add_edge(leaf, matchNode)
@@ -99,6 +108,14 @@ def verify(text, sources, pattern, indexToSourceIndex, indexToMatch):
                             curCharIndex -= 1
                             curPatternIndex -= 1
 
+                        if curPatternIndex < 0:
+                            # A match containing the entire variant -> add a new match complete node.
+                            print("  Match contains the entire variant")
+
+                            matchTree.add_node(matchNode, match_complete=True)
+                            matchTree.add_edge(leaf, matchNode)
+                            break
+
                         # Managed to traverse (match) this whole variant -> add a new continuation node.
                         if curCharIndex < 0:
                             print("  Matched the variant")
@@ -109,15 +126,20 @@ def verify(text, sources, pattern, indexToSourceIndex, indexToMatch):
                             newLeafList += [matchNode]
 
                 leafList = newLeafList
-                segmentIndex -= 1
+            segmentIndex -= 1
 
         print("Constructed a match tree")
-        assert not leafList and nx.is_directed_acyclic_graph(matchTree)
+        assert nx.is_directed_acyclic_graph(matchTree)
 
         # draw_graph(matchTree)
 
         leaves = [node for node, degree in matchTree.out_degree() if degree == 0]
+        leaves = [leaf for leaf in leaves if "match_complete" in matchTree.node[leaf]]
+
         print(f"Leaf count = {len(leaves)}")
+
+        print(matchTree.nodes(data=True))
+        print(matchTree.edges)
 
         for leaf in leaves:
             matchedSources = None
@@ -127,12 +149,15 @@ def verify(text, sources, pattern, indexToSourceIndex, indexToMatch):
                 continue
 
             for node in paths[0]:
+                if node[0] not in indexToSourceIndex:
+                    continue
+
                 newSources = set(sources[indexToSourceIndex[node[0]]][node[1]])
                 matchedSources = matchedSources & newSources if matchedSources else newSources
 
-        if matchedSources:
-            print(f"Matched sources: {matchedSources}")
-            verifiedMatches.add(matchIndex)
+            if matchedSources:
+                print(f"Matched sources: {matchedSources}")
+                verifiedMatches.add(matchIndex)
 
     print(f"Verified matches: {verifiedMatches}")
     return verifiedMatches
@@ -159,6 +184,11 @@ def main():
     assert verify(text2, sources2, "CGGACGA", indexToSourceIndex2, {2: (0, 2)}) == set([2])
     assert verify(text2, sources2, "DTCGGACG", indexToSourceIndex2, {2: (0, 1)}) == set([2])
     assert verify(text2, sources2, "GTCGGACG", indexToSourceIndex2, {2: (0, 1)}) == set()
+    assert verify(text2, sources2, "GGTCGGAA", indexToSourceIndex2, {2: (1, 0)}) == set([2])
+    assert verify(text2, sources2, "ADTCGGACGAA", indexToSourceIndex2, {2: (0, 3)}) == set([2])
+    assert verify(text2, sources2, "ADTCGGACGAAA", indexToSourceIndex2, {2: (0, 4)}) == set([2])
+    assert verify(text2, sources2, "AD", indexToSourceIndex2, {0: (0, 1)}) == set([0])
+    assert verify(text2, sources2, "ADT", indexToSourceIndex2, {0: (0, 2)}) == set([0])
 
     print("\n[Text 3]")
     text3 = [["ADT", "AC", "GGT"], ["CGGA"], ["CGAAA", "TTT", "GGG"], ["AAC", "TC"]]
@@ -167,6 +197,30 @@ def main():
 
     assert verify(text3, sources3, "GATTTAA", indexToSourceIndex3, {3: (0, 1)}) == set([3])
     assert verify(text3, sources3, "GAGGGAA", indexToSourceIndex3, {3: (0, 1)}) == set()
+    assert verify(text3, sources3, "ADTCGGACGA", indexToSourceIndex3, {2: (0, 2)}) == set([2])
+    assert verify(text3, sources3, "GGTCGGACGA", indexToSourceIndex3, {2: (0, 2)}) == set()
+
+    print("\n[Text 4]")
+    text4 = [["AA"], ["ADT", "AC", "GGT", ""], ["CGGA"], ["CGAAA", ""], ["AAC", "TC"]]
+    sources4 = [[[0], [1], [2], [3]], [[0], [1, 2, 3]], [[0, 1], [2, 3]]]
+    indexToSourceIndex4 = {1: 0, 3: 1, 4: 2}
+
+    assert verify(text4, sources4, "CGGATC", indexToSourceIndex4, {4: (0, 2)}) == set([4])
+    assert verify(text4, sources4, "ACG", indexToSourceIndex4, {2: (0, 1)}) == set([2])
+    assert verify(text4, sources4, "AACG", indexToSourceIndex4, {2: (0, 1)}) == set([2])
+    assert verify(text4, sources4, "AADTCG", indexToSourceIndex4, {2: (0, 1)}) == set([2])
+    assert verify(text4, sources4, "AAADTCG", indexToSourceIndex4, {2: (0, 1)}) == set([2])
+    assert verify(text4, sources4, "AAADTCGGACGA", indexToSourceIndex4, {3: (0, 2)}) == set([3])
+    assert verify(text4, sources4, "AAACCGGACGA", indexToSourceIndex4, {3: (0, 2)}) == set([])
+    assert verify(text4, sources4, "AAGGTCGGACGA", indexToSourceIndex4, {3: (0, 2)}) == set([])
+    assert verify(text4, sources4, "AACGGACGA", indexToSourceIndex4, {3: (0, 2)}) == set([])
+
+    print("\n[Text 5]")
+    text5 = [["ADT", "AC", "GGT"], ["CGGA", "ADT"], ["CG", "AC"]]
+    sources5 = [[[0], [1], [2]], [[0, 1], [2]], [[0, 1], [2]]]
+    indexToSourceIndex5 = {0: 0, 1: 1, 2: 2}
+
+    assert verify(text5, sources5, "ADTC", indexToSourceIndex5, {1: (0, 0), 2: (1, 0)}) == set([1, 2])
 
     print("!!All passed!!")
 
