2dc56c3b6efc7c1ff36ede46825149fe27dc537d
MrAlexSee <alex.cis91@gmail.com>
2019-10-08 18:39:10 +0200

    Extended e2e tests to include matching with sources.

--- end_to_end_tests/run_tests.sh
+++ end_to_end_tests/run_tests.sh
@@ -10,11 +10,17 @@ cd end_to_end_tests
 
 echo "3/4 Running"
 
+# Exact
 ./sopang text_test.eds patterns_test.txt > $outFile
-python3 check_result.py "2 1 1 1 1 2"
+python3 check_result.py "2 1 1 1 1 2 1 1"
 
+# Approx
 ./sopang text_test.eds patterns_test.txt -k 1 > $outFile
-python3 check_result.py "2 3 3 3 3 3"
+python3 check_result.py "2 3 3 3 3 3 1 1"
+
+# With sources
+./sopang text_test.eds patterns_test.txt -S sources_test.edss > $outFile
+python3 check_result.py "2 1 1 1 1 2 0 1"
 
 echo "4/4 Teardown"
 rm -f sopang $outFile
