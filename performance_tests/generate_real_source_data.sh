1646f249f5cfb57068e53277476978a2af076d3e
MrAlexSee <alex.cis91@gmail.com>
2019-12-27 00:32:20 +0100

    Changed real-world data script to use the new parser, some cosmetic changes.

--- performance_tests/generate_real_source_data.sh
+++ performance_tests/generate_real_source_data.sh
@@ -2,17 +2,20 @@
 
 # These paths should be relative to the root folder of this repository.
 dataDirPath="data"
-scriptsDirPath="scripts"
+scriptsDirPath="scripts/parse_fasta_vcf_cpp"
 
 inputFastaFileName="hs37d5.fa"
-maxChromosomeId=24
+maxChromosomeId=1
 
 cd ..
 mkdir -p ${dataDirPath}
+cd ${scriptsDirPath}
+make rebuild
+cd ../..
 
 for i in $(seq 1 ${maxChromosomeId});
 do
     echo $i
 
-    python3 ./${scriptsDirPath}/parse_fasta_vcf.py ${dataDirPath}/${inputFastaFileName} ${dataDirPath}/chr${i}.vcf ${dataDirPath}/chr${i}.edz ${dataDirPath}/chr${i}.edsz --compress
+    ./${scriptsDirPath}/parse_fasta_vcf ${dataDirPath}/${inputFastaFileName} ${dataDirPath}/chr${i}.vcf ${dataDirPath}/chr${i}.edz ${dataDirPath}/chr${i}.edsz
 done
--- performance_tests/run_all.sh
+++ performance_tests/run_all.sh
@@ -19,10 +19,10 @@ fi
 for i in $(seq 1 ${maxChromosomeId});
 do
     echo $i
-    ./${sopangExe} ${dataDirPath}/chr${i}.eds ${dataDirPath}/patterns8.txt -d -o ${outputFileName}_8.txt;
-    ./${sopangExe} ${dataDirPath}/chr${i}.eds ${dataDirPath}/patterns16.txt -d -o ${outputFileName}_16.txt;
-    ./${sopangExe} ${dataDirPath}/chr${i}.eds ${dataDirPath}/patterns32.txt -d -o ${outputFileName}_32.txt;
-    ./${sopangExe} ${dataDirPath}/chr${i}.eds ${dataDirPath}/patterns64.txt -d -o ${outputFileName}_64.txt;
+    ./${sopangExe} ${dataDirPath}/chr${i}.eds ${dataDirPath}/patterns8.txt -d -o ${outputFileName}_8.txt
+    ./${sopangExe} ${dataDirPath}/chr${i}.eds ${dataDirPath}/patterns16.txt -d -o ${outputFileName}_16.txt
+    ./${sopangExe} ${dataDirPath}/chr${i}.eds ${dataDirPath}/patterns32.txt -d -o ${outputFileName}_32.txt
+    ./${sopangExe} ${dataDirPath}/chr${i}.eds ${dataDirPath}/patterns64.txt -d -o ${outputFileName}_64.txt
 done
 
 echo ""
@@ -31,8 +31,8 @@ echo ""
 for i in $(seq 1 ${maxChromosomeId});
 do
     echo $i
-    ./${sopangExe} ${dataDirPath}/chr${i}.eds ${dataDirPath}/patterns8.txt -d -o ${outputFileName}_8_sources.txt --in-sources-file ${dataDirPath}/chr${i}.edss;
-    ./${sopangExe} ${dataDirPath}/chr${i}.eds ${dataDirPath}/patterns16.txt -d -o ${outputFileName}_16_sources.txt --in-sources-file ${dataDirPath}/chr${i}.edss;
-    ./${sopangExe} ${dataDirPath}/chr${i}.eds ${dataDirPath}/patterns32.txt -d -o ${outputFileName}_32_sources.txt --in-sources-file ${dataDirPath}/chr${i}.edss;
-    ./${sopangExe} ${dataDirPath}/chr${i}.eds ${dataDirPath}/patterns64.txt -d -o ${outputFileName}_64_sources.txt --in-sources-file ${dataDirPath}/chr${i}.edss;
+    ./${sopangExe} ${dataDirPath}/chr${i}.eds ${dataDirPath}/patterns8.txt -d -o ${outputFileName}_8_sources.txt --in-sources-file ${dataDirPath}/chr${i}.edss
+    ./${sopangExe} ${dataDirPath}/chr${i}.eds ${dataDirPath}/patterns16.txt -d -o ${outputFileName}_16_sources.txt --in-sources-file ${dataDirPath}/chr${i}.edss
+    ./${sopangExe} ${dataDirPath}/chr${i}.eds ${dataDirPath}/patterns32.txt -d -o ${outputFileName}_32_sources.txt --in-sources-file ${dataDirPath}/chr${i}.edss
+    ./${sopangExe} ${dataDirPath}/chr${i}.eds ${dataDirPath}/patterns64.txt -d -o ${outputFileName}_64_sources.txt --in-sources-file ${dataDirPath}/chr${i}.edss
 done
