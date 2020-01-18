6dbc6d2e60c56705abfb752b2a929986f762934a
MrAlexSee <alex.cis91@gmail.com>
2020-01-18 16:49:10 +0100

    Fixed the script for testing on synthetic data.

--- performance_tests/run_all_synthetic_data.sh
+++ performance_tests/run_all_synthetic_data.sh
@@ -6,7 +6,7 @@ outputFileName="results"
 sopangExe="sopang"
 
 minChromosomeId=25
-maxChromosomeId=29
+maxChromosomeId=28
 
 cd ..
 
@@ -19,13 +19,13 @@ fi
 # Running regular ED-text matching.
 for i in $(seq ${minChromosomeId} ${maxChromosomeId});
 do
-    ./${sopangExe} ${dataDirPath}/chr${i}.eds ${dataDirPath}/patterns8.txt -d -o ${outputFileName}_8.txt --in-compressed
+    ./${sopangExe} ${dataDirPath}/chr${i}.eds ${dataDirPath}/patterns8.txt -d -o ${outputFileName}_8.txt
 
-    ./${sopangExe} ${dataDirPath}/chr${i}.eds ${dataDirPath}/patterns16.txt -d -o ${outputFileName}_16.txt --in-compressed
+    ./${sopangExe} ${dataDirPath}/chr${i}.eds ${dataDirPath}/patterns16.txt -d -o ${outputFileName}_16.txt
 
-    ./${sopangExe} ${dataDirPath}/chr${i}.eds ${dataDirPath}/patterns32.txt -d -o ${outputFileName}_32.txt --in-compressed
+    ./${sopangExe} ${dataDirPath}/chr${i}.eds ${dataDirPath}/patterns32.txt -d -o ${outputFileName}_32.txt
 
-    ./${sopangExe} ${dataDirPath}/chr${i}.eds ${dataDirPath}/patterns64.txt -d -o ${outputFileName}_64.txt --in-compressed
+    ./${sopangExe} ${dataDirPath}/chr${i}.eds ${dataDirPath}/patterns64.txt -d -o ${outputFileName}_64.txt
 done
 
 # Running ED-text matching with sources.
@@ -33,16 +33,16 @@ for i in $(seq ${minChromosomeId} ${maxChromosomeId});
 do
     for sourceCount in 16 128 1024 8192 16000
     do
-        ./${sopangExe} ${dataDirPath}/chr${i}.eds ${dataDirPath}/patterns8.txt -d -o ${outputFileName}_8_sources.txt --in-sources-file ${dataDirPath}/chr${i}_${sourceCount}.edss --in-compressed
-        ./${sopangExe} ${dataDirPath}/chr${i}.eds ${dataDirPath}/patterns8.txt -d -o ${outputFileName}_8_sources.txt --in-sources-file ${dataDirPath}/chr${i}_${sourceCount}.edss --in-compressed --full-sources-output
+        ./${sopangExe} ${dataDirPath}/chr${i}.eds ${dataDirPath}/patterns8.txt -d -o ${outputFileName}_8_sources.txt --in-sources-file ${dataDirPath}/chr${i}_${sourceCount}.edss
+        ./${sopangExe} ${dataDirPath}/chr${i}.eds ${dataDirPath}/patterns8.txt -d -o ${outputFileName}_8_sources.txt --in-sources-file ${dataDirPath}/chr${i}_${sourceCount}.edss --full-sources-output
 
-        ./${sopangExe} ${dataDirPath}/chr${i}.eds ${dataDirPath}/patterns16.txt -d -o ${outputFileName}_16_sources.txt --in-sources-file ${dataDirPath}/chr${i}_${sourceCount}.edss --in-compressed
-        ./${sopangExe} ${dataDirPath}/chr${i}.eds ${dataDirPath}/patterns16.txt -d -o ${outputFileName}_16_sources.txt --in-sources-file ${dataDirPath}/chr${i}_${sourceCount}.edss --in-compressed --full-sources-output
+        ./${sopangExe} ${dataDirPath}/chr${i}.eds ${dataDirPath}/patterns16.txt -d -o ${outputFileName}_16_sources.txt --in-sources-file ${dataDirPath}/chr${i}_${sourceCount}.edss
+        ./${sopangExe} ${dataDirPath}/chr${i}.eds ${dataDirPath}/patterns16.txt -d -o ${outputFileName}_16_sources.txt --in-sources-file ${dataDirPath}/chr${i}_${sourceCount}.edss --full-sources-output
 
-        ./${sopangExe} ${dataDirPath}/chr${i}.eds ${dataDirPath}/patterns32.txt -d -o ${outputFileName}_32_sources.txt --in-sources-file ${dataDirPath}/chr${i}_${sourceCount}.edss --in-compressed
-        ./${sopangExe} ${dataDirPath}/chr${i}.eds ${dataDirPath}/patterns32.txt -d -o ${outputFileName}_32_sources.txt --in-sources-file ${dataDirPath}/chr${i}_${sourceCount}.edss --in-compressed --full-sources-output
+        ./${sopangExe} ${dataDirPath}/chr${i}.eds ${dataDirPath}/patterns32.txt -d -o ${outputFileName}_32_sources.txt --in-sources-file ${dataDirPath}/chr${i}_${sourceCount}.edss
+        ./${sopangExe} ${dataDirPath}/chr${i}.eds ${dataDirPath}/patterns32.txt -d -o ${outputFileName}_32_sources.txt --in-sources-file ${dataDirPath}/chr${i}_${sourceCount}.edss --full-sources-output
 
-        ./${sopangExe} ${dataDirPath}/chr${i}.eds ${dataDirPath}/patterns64.txt -d -o ${outputFileName}_64_sources.txt --in-sources-file ${dataDirPath}/chr${i}_${sourceCount}.edss --in-compressed
-        ./${sopangExe} ${dataDirPath}/chr${i}.eds ${dataDirPath}/patterns64.txt -d -o ${outputFileName}_64_sources.txt --in-sources-file ${dataDirPath}/chr${i}_${sourceCount}.edss --in-compressed --full-sources-output
+        ./${sopangExe} ${dataDirPath}/chr${i}.eds ${dataDirPath}/patterns64.txt -d -o ${outputFileName}_64_sources.txt --in-sources-file ${dataDirPath}/chr${i}_${sourceCount}.edss
+        ./${sopangExe} ${dataDirPath}/chr${i}.eds ${dataDirPath}/patterns64.txt -d -o ${outputFileName}_64_sources.txt --in-sources-file ${dataDirPath}/chr${i}_${sourceCount}.edss --full-sources-output
     done
 done
